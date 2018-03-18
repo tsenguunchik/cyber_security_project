27444b3f0628baff6a9717632ab1abaa4ae041e0
Tsenguun Tsogbadrakh <tsenguun@uw.edu>
2018-03-17 20:58:02 -0400

    Added insert and replace methods

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 import time
 from . import apps
-
+import random
 # from . import serializers
 
 # Create your views here.
@@ -15,10 +15,10 @@ class HelloApiView(APIView):
 
     # serializer_class = serializers.HelloSerializer
 
-    def get(self, request, format=None):
-        """Returns a list of APIView features."""
+
+    def check_password(self, sent_password):
         start_time = time.time()
-        sent_password = request.GET.get('password', '')
+
 
         trie_tree = apps.get_top_1000()
         password_set = {}
@@ -42,7 +42,76 @@ class HelloApiView(APIView):
                     response = {'is_safe': 'Yes', 'message': 'Password was not found in database'}
 
         response['time'] = str(int((time.time() - start_time) * 1000000)) + ' us'
+        return response
+
+    def change_password(self, password, type, number, tries):
+        new_password = list(password)
+        try:
+            number = int(number)
+        except:
+            return {'error': 'Number must be integer'}
+        if 1 > number or number > 10:
+            return {'error': 'Number must be from 1 to 10'}
+        all_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$^*().,0123456789'
+        numbers_chars = '0123456789'
+        alpha_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        special_chars = '!@$^*().,'
+        tries = tries + 1
+        if type == 'insert':
+            for i in range(number):
+                new_password.insert(random.randint(0, len(new_password)), random.choice(all_chars))
+        elif type == 'replace':
+            for i in range(number):
+                new_password[random.randint(0, len(new_password) - 1)] = random.choice(all_chars)
+        else:
+            return {'error': 'Unknown Type'}
+
+        new_password = ''.join(new_password)
+
+
+        if self.check_password(new_password)['is_safe'] == 'Yes':
+            return {'new_password': new_password, 'tries': str(tries), 'is_safe': 'Yes'}
+        else:
+            while self.check_password(new_password)['is_safe'] == 'No' and tries < 10:
+                tries += 1
+                new_password = self.change_password(password, type, number, tries)['new_password']
+            if tries == 10:
+                return {'new_password': new_password, 'tries': str(tries), 'is_safe': 'No'}
+            else:
+                return {'new_password': new_password, 'tries': str(tries), 'is_safe': 'Yes'}
+
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features."""
+
+        sent_password = request.GET.get('password', '')
+        type = request.GET.get('type', '')
+        number = request.GET.get('number', '')
+        response = {}
+        if type == '' and number == '':
+            response = self.check_password(sent_password)
+        else:
+            response = self.change_password(sent_password, type, number, 0)
+
         return Response(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # def post(self, request):
     #     """Create a hello message with our name."""
     #
